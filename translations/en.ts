d7eedf80456b6e1eda7c12c6bade965dff38dbee
Bill Neubauer <bill.neubauer.4@gmail.com>
2024-09-27 02:08:39 -0400

    Adding initial stuff for new Annotation Engine stuff, now supporting multi-lingual translations

---
+++ translations/en.ts
@@ -0,0 +1,91 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en">
+  <context>
+    <name>AnnotationApp</name>
+    <message>
+      <source>Sign Language Annotation Tool</source>
+      <translation>Sign Language Annotation Tool</translation>
+    </message>
+    <message>
+      <source>Select Review Batch:</source>
+      <translation>Select Review Batch:</translation>
+    </message>
+    <message>
+      <source>Select Word:</source>
+      <translation>Select Word:</translation>
+    </message>
+    <message>
+      <source>Video Display Area</source>
+      <translation>Video Display Area</translation>
+    </message>
+    <message>
+      <source>Play</source>
+      <translation>Play</translation>
+    </message>
+    <message>
+      <source>Pause</source>
+      <translation>Pause</translation>
+    </message>
+    <message>
+      <source>Replay</source>
+      <translation>Replay</translation>
+    </message>
+    <message>
+      <source>Speed Up</source>
+      <translation>Speed Up</translation>
+    </message>
+    <message>
+      <source>Slow Down</source>
+      <translation>Slow Down</translation>
+    </message>
+    <message>
+      <source>Next</source>
+      <translation>Next</translation>
+    </message>
+    <message>
+      <source>Previous</source>
+      <translation>Previous</translation>
+    </message>
+    <message>
+      <source>Good</source>
+      <translation>Good</translation>
+    </message>
+    <message>
+      <source>Variant</source>
+      <translation>Variant</translation>
+    </message>
+    <message>
+      <source>Unrecognizable</source>
+      <translation>Unrecognizable</translation>
+    </message>
+    <message>
+      <source>Further Review</source>
+      <translation>Further Review</translation>
+    </message>
+    <message>
+      <source>Help</source>
+      <translation>Help</translation>
+    </message>
+    <message>
+      <source>Reference Video</source>
+      <translation>Reference Video</translation>
+    </message>
+    <message>
+      <source>Reference Video for Annotation</source>
+      <translation>Reference Video for Annotation</translation>
+    </message>
+    <message>
+      <source>Play Reference Video</source>
+      <translation>Play Reference Video</translation>
+    </message>
+    <message>
+      <source>Info</source>
+      <translation>Info</translation>
+    </message>
+    <message>
+      <source>No more videos in this batch.</source>
+      <translation>No more videos in this batch.</translation>
+    </message>
+  </context>
+</TS>
