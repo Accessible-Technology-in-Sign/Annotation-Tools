edbcc4054d112a97a4173e7c71820ec93fd9cb7b
Sahir Shahryar <sahirshahryar@gmail.com>
2024-10-17 18:58:11 -0400

    Add scaffolding for web-based annotation engine

---
+++ WebAnnotationEngine/src/routes/+layout.svelte
@@ -0,0 +1,25 @@
+<script>
+	import '../app.css';
+</script>
+
+<div class="drawer lg:drawer-open">
+  <input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
+  <div class="drawer-content flex flex-col items-center justify-center">
+    <slot />
+    <label for="my-drawer-2" class="btn btn-primary drawer-button lg:hidden">
+      Open drawer
+    </label>
+  </div>
+  <div class="drawer-side">
+    <label for="my-drawer-2" aria-label="close sidebar" class="drawer-overlay"></label>
+    <ul class="menu bg-base-200 text-base-content min-h-full w-20 p-4">
+      <img class="size-20" src="logo.svg" />
+      <!-- Sidebar content here -->
+      <li id="review-batch">
+          <a><img class="size-20" src="review-batch.svg"></a>
+      </li>
+      <li><a><img class="size-20" src="sign.svg"></a></li>
+      <li><a><img class="size-20" src="comment.svg"></a></li>
+    </ul>
+  </div>
+</div>
---
+++ WebAnnotationEngine/src/routes/+page.svelte
@@ -0,0 +1,27 @@
+<script>
+  import { Pane, Splitpanes } from 'svelte-splitpanes';
+</script>
+
+<!-- Video viewer -->
+<div class="h-full w-full">
+    <!-- See svelte-splitpanes https://orefalo.github.io/svelte-splitpanes/ -->
+    <Splitpanes class="p-4" style="height: 100%">
+      <Pane class="rounded-xl" minSize={20}>
+        <!-- Video to review -->
+        <video class="w-full h-full" src="ReviewVideos/all_work_and_no_play_rh_wire.mp4"
+            controls
+            loop />
+      </Pane>
+      <Pane class="rounded-xl" minSize={15}>
+        <!-- Reference video -->
+        <video class="w-full h-full"
+            src="ReviewVideos/all_work_and_no_play_rh_wire.mp4"
+            controls loop />
+      </Pane>
+    </Splitpanes>
+</div>
+
+<!-- Video controls -->
+<div class="w-full h-20">
+
+</div>
