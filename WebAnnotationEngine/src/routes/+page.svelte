67a2a57a3515399a076b9b29fa1d1c2c5476e1ec
Sahir Shahryar <sahirshahryar@gmail.com>
2024-10-28 23:21:28 -0400

    Fix button click example

--- WebAnnotationEngine/src/routes/+page.svelte
+++ WebAnnotationEngine/src/routes/+page.svelte
@@ -2,11 +2,13 @@
   import { Pane, Splitpanes } from 'svelte-splitpanes';
   let reviewVideoPaused = true;
 
+  // Toggle playback of the video to be reviewed. You could add something similar for
+  // the reference video too.
   function playPause() {
     reviewVideoPaused = !reviewVideoPaused;
   }
 
-
+  // Handle key press events for keybinds (e.g., play/pause, approve/reject, etc.)
   function onKeyPress(event) {
     switch (event.key) {
       case " ":
@@ -26,7 +28,6 @@
       <Pane class="rounded-xl" minSize={20}>
         <!-- Video to review -->
         <video class="w-full h-full" src="ReviewVideos/all_work_and_no_play_rh_wire.mp4"
-            controls
             loop
             bind:paused={reviewVideoPaused} />
       </Pane>
@@ -41,7 +42,7 @@
 
 <!-- Video controls -->
 <div class="w-full h-20 flex items-center justify-start">
-  <button onclick={playPause} class="bg-[#D9D9D9] hover:bg-[#A9A9A9] text-white rounded-md p-2 md:p-2 lg:p-2.5 xl:p-3 m-3">
+  <button on:click={playPause} class="bg-[#D9D9D9] hover:bg-[#A9A9A9] text-white rounded-md p-2 md:p-2 lg:p-2.5 xl:p-3 m-3">
     <img class="w-5 h-5 md:w-6 md:h-6 lg:w-7 lg:h-7 xl:w-8 xl:h-8" src="pause.svg">
   </button>
 
