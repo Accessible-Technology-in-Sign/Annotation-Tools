7fbf82e4e739d56cece76e796e4ac412348d6ab9
Eris Wang <eriswang2304@gmail.com>
2024-10-30 01:13:40 -0400

    playback speed functionality
    added functionality and key binds to slow down and speed up buttons

--- WebAnnotationEngine/src/routes/+page.svelte
+++ WebAnnotationEngine/src/routes/+page.svelte
@@ -1,5 +1,67 @@
 <script>
   import { Pane, Splitpanes } from 'svelte-splitpanes';
+  let videoElement;
+  let reviewVideoPaused = true;
+  let reviewVideoLooped = true;
+  let playbackRate = 1;
+
+  // Toggle playback of the video to be reviewed. You could add something similar for
+  // the reference video too.
+  function playPause() {
+    reviewVideoPaused = !reviewVideoPaused;
+    document.getElementById("playPauseIcon").src = reviewVideoPaused ? "play.svg" : "pause.svg";
+  }
+
+  function letLoop() {
+    reviewVideoLooped = !reviewVideoLooped;
+    videoElement.loop = reviewVideoLooped;
+    document.getElementById("loopIcon").src = reviewVideoLooped ? "not-looped.svg" : "loop.svg";
+  }
+
+  function handleVideoEnded() {
+    !reviewVideoPaused;
+    document.getElementById("playPauseIcon").src = "play.svg";
+  }
+
+  function slowDown() {
+    playbackRate = Math.max(0.25, playbackRate - 0.25);
+  }
+
+  function speedUp() {
+    playbackRate = Math.min(2, playbackRate + 0.25);
+  }
+
+  // Handle key press events for keybinds (e.g., play/pause, approve/reject, etc.)
+  function onKeyPress(event) {
+    switch (event.key) {
+      case " ":
+        playPause();
+        event.preventDefault();
+        break;
+    }
+
+    switch (event.key) {
+      case "-":
+        letLoop();
+        event.preventDefault();
+        break;
+    }
+
+    switch (event.key) {
+      case "[":
+        slowDown();
+        event.preventDefault();
+        break;
+    }
+
+    switch (event.key) {
+      case "]":
+        speedUp();
+        event.preventDefault();
+        break;
+    }
+
+  }
 </script>
 
 <!-- Video viewer -->
@@ -9,8 +71,11 @@
       <Pane class="rounded-xl" minSize={20}>
         <!-- Video to review -->
         <video class="w-full h-full" src="ReviewVideos/all_work_and_no_play_rh_wire.mp4"
-            controls
-            loop />
+            loop
+            bind:this={videoElement}
+            bind:paused={reviewVideoPaused}
+            on:ended={handleVideoEnded}
+            bind:playbackRate={playbackRate} />
       </Pane>
       <Pane class="rounded-xl" minSize={15}>
         <!-- Reference video -->
@@ -31,11 +96,13 @@
     <img class="w-5 h-5 md:w-6 md:h-6 lg:w-7 lg:h-7 xl:w-8 xl:h-8" src="loop.svg">
   </button>
 
-  <button class="bg-[#D9D9D9] hover:bg-[#A9A9A9] text-white rounded-md p-2 md:p-2 lg:p-2.5 xl:p-3 m-3">
+  <!-- Slow down video button-->
+  <button on:click={slowDown} class="bg-[#D9D9D9] hover:bg-[#A9A9A9] text-white rounded-md p-2 md:p-2 lg:p-2.5 xl:p-3 m-3">
     <img class="w-8 h-8 md:w-7.5 md:h-7.5 lg:w-8 lg:h-8 xl:w-9 xl:h-9" src="slow-down-dark.svg">
   </button>
 
-  <button class="bg-[#D9D9D9] hover:bg-[#A9A9A9] text-white rounded-md p-2 md:p-2 lg:p-2.5 xl:p-3 m-3">
+  <!-- Speed up video button-->
+  <button on:click={speedUp} class="bg-[#D9D9D9] hover:bg-[#A9A9A9] text-white rounded-md p-2 md:p-2 lg:p-2.5 xl:p-3 m-3">
     <img class="w-5 h-5 md:w-6 md:h-6 lg:w-7 lg:h-7 xl:w-8 xl:h-8" src="speed-up-dark.svg">
   </button>
 </div>
