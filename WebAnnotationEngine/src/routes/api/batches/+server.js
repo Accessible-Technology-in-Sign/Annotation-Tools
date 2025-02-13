a5be3d7afe3d73815f704ee3be65feba2270a3d1
bzhang625 <bzhang625@gatech.edu>
2025-02-13 14:55:12 -0500

    filter words with sign_list.txt

--- WebAnnotationEngine/src/routes/api/batches/+server.js
+++ WebAnnotationEngine/src/routes/api/batches/+server.js
@@ -2,6 +2,7 @@
 Put videoConfig.json in src/routes/config 
 Put review videos like this: ReviewVideos/BatchName/WordName/all the videos
 Put all reference videos in 1 folder
+Put sign_list.txt in src/routes/config and list words to annotate or leave enpty if want all
 
 example videoConfig.json:
 {
@@ -18,28 +19,41 @@ import path from 'path';
 
 export async function GET() {
   const configPath = path.resolve('src/routes/config/videoConfig.json');
+  const signListPath = path.resolve('src/routes/config/sign_list.txt');
 
   try {
     const configData = JSON.parse(fs.readFileSync(configPath, 'utf-8'));
+    const signListData = fs.readFileSync(signListPath, 'utf-8');
 
     const reviewSource = path.resolve(configData.review_source)
     const referenceSource = path.resolve(configData.reference_source);
     const batchesToLoad = configData.batches || [];
     const batches = {};
+
     const relativePath = (fullPath) => fullPath.replace(path.resolve('static'), '');
 
+    const signListByLine = signListData.split("\n")
+
+    //If batches specified, only include those
     const batchDirs = batchesToLoad.length > 0
       ? batchesToLoad.map(batch => ({ name: batch }))
       : fs.readdirSync(reviewSource, { withFileTypes: true }).filter(dir => dir.isDirectory());
 
+    //create entry in batch object for the batch 
     for (const batchDir of batchDirs) {
       const batchName = batchDir.name;
       const batchPath = path.join(reviewSource, batchName);
       batches[batchName] = {};
-
+      
       const signDirs = fs.readdirSync(batchPath, { withFileTypes: true }).filter(dir => dir.isDirectory());
 
+      //add all signs words in batch 
       for (const signDir of signDirs) {
+        //if sign list contains words, skip those not specified
+        if (signListData.trim() != "" && !signListByLine.includes(signDir.name)) {
+          console.log("skipped: " + signDir.name);
+          continue;
+        }
         const signName = signDir.name;
         const signPath = path.join(batchPath, signName);
         const videos = fs.readdirSync(signPath).filter(file => file.endsWith('.mp4'));
@@ -48,16 +62,25 @@ export async function GET() {
           batches[batchName][signName] = { reference: null, reviews: [] };
         }
 
+        //add all videos to each sign
         for (const file of videos) {
           const filePath = path.join(signPath, file);
           batches[batchName][signName].reviews.push(relativePath(filePath));
         }
       }
+      console.log(batchName + " length: " + Object.keys(batches[batchName]).length);
+      if (Object.keys(batches[batchName]).length == 0) {
+        delete batches[batchName];
+      }
     }
     const referenceFiles = fs.readdirSync(referenceSource).filter(file => file.endsWith('.mp4'));
         
-
+    //reference videos
     for (const file of referenceFiles) {
+      //if sign list contains words, skip those not specified
+      if (signListData.trim() != "" && !signListByLine.includes(path.parse(file).name)) {
+        continue;
+      }
       const signName = path.parse(file).name;
       const referencePath = path.join(referenceSource, file);
 
