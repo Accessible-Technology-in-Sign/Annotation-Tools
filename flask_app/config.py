ed79cc4d1d3508e469008bde08cbd2dfc1506c5e
bzhang625 <bzhang625@gatech.edu>
2025-04-01 17:18:01 -0400

    change

--- flask_app/config.py
+++ flask_app/config.py
@@ -1,8 +1,8 @@
 import os
 
 class DBLogin:
-    USER = "root"
-    PSWD = "netherportais510"
+    USER = "user"
+    PSWD = "pswd"
 
 class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv(f"mysql+pymysql://{DBLogin.USER}:{DBLogin.PSWD}@localhost/labels", "sqlite:///database.db")
