178d2337615d7cf33d3910432073c5ab1c9bf150
ccai73 <christinay_cai@yahoo.com>
2025-11-20 17:45:45 -0500

    Add tutorial button in side bar, tutorial outline with login/selection page images

---
+++ WebAnnotationEngine/src/routes/tutorial/+page.svelte
@@ -0,0 +1,65 @@
+<!-- TODO: Add the new updates (progress bar, annotation summary, etc.) & all images -->
+
+<div class="p-8 prose max-w-3xl mx-auto">
+  <h1 class="text-3xl font-bold mb-4">Annotation Engine Tutorial</h1>
+
+  <p>Welcome 👋! This page shows how to label videos. You help us teach sign language!</p>
+
+  <h2 class="text-2xl mt-6">Login</h2>
+  <p>Enter the username provided by the researcher, and press start.</p>
+  <img src="/tutorial/login-page.png" alt="Login page" class="w-1/2" />
+
+
+  <h2 class="text-2xl mt-6">Selection Page</h2>
+  <img src="/tutorial/selection-page.png" alt="Selection page" class="w-full" />
+  <p>Click on your assigned batch, and the words will appear in the right panel.</p>
+  <p>Select the first word in the list to begin annotating!</p>
+
+  <h2 class="text-2xl mt-6">Annotation Page</h2>
+  <img src="/tutorial/annotation-page.png" alt="Annotation page" class="w-full" />
+  <p>The video on the left is the review video and the video on the top right is the reference video.
+    Your goal is to classify the review videos as <span style="color: ForestGreen;">Good</span>, <span style="color: GoldenRod;">Variant</span>, <span style="color: FireBrick;">Bad</span>, or <span style="color: CornflowerBlue;">Further Review</span>.
+  </p>
+
+  <h3 class="text-xl mt-4">Video Controls</h3>
+  <ul class="list-disc list-inside">
+    <li>Pause/unpause (<kbd>space</kbd>)</li>
+    <li>Toggle replay (<kbd>-</kbd>)</li>
+    <li>Playback speed: Turtle (<kbd>[</kbd>), Rabbit (<kbd>]</kbd>)</li>
+    <li>Previous video (<kbd>0</kbd>)</li>
+    <li>Next video (<kbd>=</kbd>)</li>
+  </ul>
+
+  <h3 class="text-xl mt-4">Annotation Labels</h3>
+  <ul class="list-disc list-inside">
+    <li>Good (<kbd>z</kbd>)</li>
+    <li>Variant (<kbd>x</kbd>)</li>
+    <li>Bad (<kbd>c</kbd>)</li>
+    <li>Further Review (<kbd>v</kbd>)</li>
+  </ul>
+
+  <p>
+    Once labeled, go to the next video. You can always return to a video to relabel it.
+  </p>
+
+  <p>
+    This button in the top right allows you to show/hide the reference video.
+  </p>
+
+  <p>
+    This button in the top left allows you to return to the batch selection page.
+  </p>
+
+  <p>
+    Type comments about a review video in the comment box under the reference video:
+  </p>
+
+  <p>
+    When you finish a word, a popup will take you back to the batch selection page.
+  </p>
+
+  <h2 class="text-2xl mt-6">Log out</h2>
+  <p>Once you have finished all words in the batch, you may log out with this button in the batch selection page:</p>
+
+  <p class="mt-8 text-lg font-semibold">Happy annotating! 👏</p>
+</div>
